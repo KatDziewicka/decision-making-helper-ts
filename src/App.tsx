57440d77e1b24b2fc0ccaf7b2db541fb1b68231e
Katarzyna Dziewicka <dziewicka.k@gmail.com>
2022-02-09 15:16:35 +0100

    Create sliders for scoring a choice

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BasicFlow from './components/Flow';
+import IndividualScores from './components/IndividualScores';
 
 function App() {
   return (
-    <BasicFlow />
+    <IndividualScores />
   );
 }
 
